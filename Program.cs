671d9344a685de61c57d0c785053e6711f01fbdf
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 17:40:16 -0500

    Add files via upload
    I created a array sort method to sort the elements in the array.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array_Sort_Method
+{
+    class simple_array_sort_method
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = {100,
+                                7,
+                                92,
+                                53,
+                                89,
+                                8
+
+                            };
+            Array.Sort(numbers);
+
+            foreach (int val in numbers)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
